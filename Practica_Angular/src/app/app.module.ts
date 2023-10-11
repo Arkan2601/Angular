bb1e1b8a29fea52deff73b3f4f7ba2189d0e123f
Jonathan Cortes <124106713+Arcan2601@users.noreply.github.com>
2023-10-11 08:13:45 -0600

    v2

--- Practica_Angular/src/app/app.module.ts
+++ Practica_Angular/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{RouterModule, Routes} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+
+
+const appRoutes: Routes=[
+  {path:'inicio',component:InicioComponent},
+  {path:'nosotros',component:NosotrosComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing:true}
+    ),
     BrowserModule,
     AppRoutingModule
   ],
