584755b34f0008f90cc503b54da206a02095a773
Jonathan Cortes <124106713+Arcan2601@users.noreply.github.com>
2023-10-12 08:54:52 -0600

    v3

--- Practica_Angular/src/app/app.module.ts
+++ Practica_Angular/src/app/app.module.ts
@@ -7,7 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { LoginComponent } from './login/login.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 const appRoutes: Routes=[
   {path:'inicio',component:InicioComponent},
@@ -18,7 +26,8 @@ const appRoutes: Routes=[
   declarations: [
     AppComponent,
     InicioComponent,
-    NosotrosComponent
+    NosotrosComponent,
+    LoginComponent
   ],
   imports: [
     RouterModule.forRoot(
@@ -26,7 +35,15 @@ const appRoutes: Routes=[
       {enableTracing:true}
     ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule, 
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Practica_Angular/src/app/inicio/inicio.component.ts
+++ Practica_Angular/src/app/inicio/inicio.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
+  
 })
-export class InicioComponent {
-
-}
+export class InicioComponent {}
+export class ToolbarOverviewExample {}
